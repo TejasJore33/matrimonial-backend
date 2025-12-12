1a530e65dcad44f91ca8e9c438bcf7fb594d7e2e
Tejas Jore <116269768+TejasJore33@users.noreply.github.com>
2025-12-12 23:25:11 +0530

    .

--- prisma/prisma.config.ts
+++ prisma/prisma.config.ts
@@ -1,12 +1,6 @@
-// import { PrismaClient } from "@prisma/client";
-
-// export const prisma = new PrismaClient({
-//   datasources: {
-//     db: {
-//       // either use a direct database URL:
-//       url: process.env.DATABASE_URL,
-//       // or for Prisma Accelerate:npx prisma migrate dev
-//       // accelerateUrl: process.env.ACCELERATE_URL,
-//     },
-//   },
-// });
+export default {
+  datasource: {
+    url: process.env.DATABASE_URL,
+    directUrl: process.env.DIRECT_URL || process.env.DATABASE_URL,
+  },
+};
